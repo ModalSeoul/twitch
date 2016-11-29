5f2c9b26291552664f365b895e24d1d41cf9d0f3
ModalSeoul <algernon@teknik.io>
2016-11-29 08:21:23 -0700

    scan_channel and get_chatters

--- viewbot.py
+++ viewbot.py
@@ -10,10 +10,24 @@ class Twitch:
             'Authorization': 'OAuth {}'.format(oauth)
         }
 
+    def get_chatters(self, user):
+        r = requests.get('http://tmi.twitch.tv/group/user/%s/chatters'%user)
+        return r.json()['chatters']['viewers']
+
     def request_user(self, user):
         r = requests.get('{}{}'.format(self._user, user), headers=self._headers)
         if r.status_code == 200:
-            user_data = r.json()
+            return r.json()
+        else:
+            return None
+
+    def scan_channel(self, channel):
+        chatters = self.get_chatters(channel)
+        outfile = open('')
+        for chatter in chatters:
+            user = self.request_user(chatter)
+            if user:
+                user = user['created_at']
 
 twitch = Twitch('your_oauth')
-twitch.request_user('jellypeanut')
+twitch.scan_channel('nahj')
