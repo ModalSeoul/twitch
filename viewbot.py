50a14ce7034714762fda7145b417a9e567626608
ModalSeoul <algernon@teknik.io>
2016-11-30 05:00:32 -0700

    K this works now

--- viewbot.py
+++ viewbot.py
@@ -1,11 +1,12 @@
 import requests
 import json
+import operator
 
 
 class Twitch:
 
     def __init__(self, oauth):
-        self._user = 'https://api.twitch.tv/kraken/users/';
+        self._user = 'https://api.twitch.tv/kraken/users/'
         self._headers = {
             'Authorization': 'OAuth {}'.format(oauth)
         }
@@ -23,20 +24,44 @@ class Twitch:
 
     def scan_channel(self, channel):
         chatters = self.get_chatters(channel)
-        outfile = open(channel, 'w+')
-        for chatter in chatters:
+        self.outfile = open(channel, 'w+')
+
+        for indx, chatter in enumerate(chatters):
             user = self.parse_date(self.request_user(chatter)['created_at'])
             if user:
                 outfile.write('{}{}'.format(user, '\n'))
+                print(indx)
+        self.evaluate_scan(channel)
+
+    def evaluate_scan(self, channel):
+        with open(channel, 'r+') as infile:
+            entries = infile.readlines()
+            evaluation = {}
+
+            for entry in entries:
+                if entry in evaluation:
+                    evaluation[entry] += 1
+                else:
+                    evaluation[entry] = 1
+
+        most_common = max(evaluation, key=lambda i: evaluation[i])
+        json.dump(
+            evaluation, open(channel + '.json', 'w'), indent=4, sort_keys=False)
+        print('{} : {}'.format(
+            self.remove_break(most_common), evaluation[most_common]))
 
     # ---------
     # Utility
     # ---------
 
     def parse_date(self, str_date):
-        return str_date.split('T')[0]
+        return str_date.split('T')[0].replace('\n', '')
+
+    def remove_break(self, string):
+        return string.replace('\n', '')
 
 if __name__ == '__main__':
     channel = input('Enter channel to scan\n--------\n')
     twitch = Twitch('your_oauth')
     twitch.scan_channel(channel)
+    twitch.evaluate_scan(channel)
